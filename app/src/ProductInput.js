6b87c1799748497072fb566886e10d982da4f23e
GUELIANEBelkacem <47327501+GUELIANEBelkacem@users.noreply.github.com>
2022-04-05 21:47:30 +0200

    Update ProductInput.js
    cor5

--- app/src/ProductInput.js
+++ app/src/ProductInput.js
@@ -44,7 +44,7 @@ const ProductInput = ({callback, userID})  =>{
             
         };
 
-        fetch('http://localhost:8000/users/submit', requestOptions)
+        fetch('http://0.0.0.0:8000/users/submit', requestOptions)
         /*
         .then(async response => {
             const isJson = response.headers.get('content-type')?.includes('application/json');
